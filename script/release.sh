11c5a80abac6b69cbeb49d3ecd03fd3866990a3c
3y3 <3y3@ya.ru>
2024-04-05 15:07:18 +0300

    Update release process

--- script/release.sh
+++ script/release.sh
@@ -2,10 +2,25 @@
 
 set -e
 
+VERSION=$1
+
 export NODE_ENV=production
 
 npm i @diplodoc/cli@latest --no-workspaces
 
-VERSION=$(node -pe 'require("./package.json").dependencies["@diplodoc/cli"]')
+CLI_VERSION=$(node -pe 'require("./package.json").dependencies["@diplodoc/cli"]')
 
-git commit -am "Update cli to version $VERSION"
+if [[ -n "$(git status -s | grep package.json)" ]]; then
+  git add package.json package-lock.json
+  git commit -m "Update cli to version $CLI_VERSION"
+  git push
+fi
+
+if [[ -n "$(git status -s)" ]]; then
+  echo "Unable to release unstaged changes"
+  git status -s
+  exit 1
+fi
+
+git tag v$VERSION
+git push origin v$VERSION
